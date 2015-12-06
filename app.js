0b87a9edda2503edd46f5b2f589dd74fa53a4f8f
williamjehanne <william.jehanne@gmail.com>
2015-12-06 22:10:27 +0100

    add widget component

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ var App = React.createClass({
             cat: 0
         }
     },
+    getInitialState:function(){
+        return {
+            montxt: 'the state txt',
+            id: 0
+        }
+    },
+    update: function(e) {
+        this.setState({montxt: e.target.value});
+    },
     propTypes: {
         txt: React.PropTypes.string,
         cat: React.PropTypes.number.isRequired
@@ -13,11 +22,23 @@ var App = React.createClass({
         var txt = this.props.txt;
         return (
             <div>
-            <b>{txt}</b>
-            <h1>Hello world !</h1>
-        </div>
+                <b>{txt}</b>
+                <Widget montxt={this.state.montxt} update={this.update}/>
+            </div>
         )
     }
 });
+
+var Widget = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <h1>Hello world !</h1>
+                <input type="text" onChange={this.props.update}></input>
+                <h1>{this.props.montxt}</h1>
+            </div>
+        )
+    }
+})
 
 React.render(<App cat={5} txt="this is the props txt" />, document.body);
